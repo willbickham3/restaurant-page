e14dd7bb41fd585ea2afa890e3e9e305330757a3
Will <willbickham3@gmail.com>
2023-07-21 15:26:56 -0400

    import createTabs function for readability; call homePage

--- src/startup.js
+++ src/startup.js
@@ -1,32 +1,34 @@
+import homePage from './home';
+import createTabsWithImage from './tabCreater'
+import banner from '/src/images/banner.png'
+
+// createTabsWithImage takes 3 arguments (inner text of button, class of button, and image src)
+// page load module
+
 function tabDisplay() {
     const content = document.querySelector('#content');
 
     let navHeader = document.createElement('header');
     const tabContainer = document.createElement('nav');
     tabContainer.classList.add('tabContainer');
+
+    let bannerBottom = document.createElement('img');
+    bannerBottom.src = banner;
     
     document.body.prepend(navHeader)
     document.querySelector('header').appendChild(tabContainer);
 
-    const menuBtn = document.createElement('Button');
-    menuBtn.innerHTML = 'Menu';
-    menuBtn.classList.add('navButton');
-
-    const homeBtn = document.createElement('Button');
-    homeBtn.innerHTML = 'Home';
-    homeBtn.classList.add('navButton');
-
-    const contactBtn = document.createElement('Button');
-    contactBtn.innerHTML = 'Contact Us';
-    contactBtn.classList.add('navButton');
+    const menuBtn = createTabsWithImage('Menu', 'navButton', banner);
+    const homeBtn = createTabsWithImage('Home', 'navButton', banner);
+    const contactBtn = createTabsWithImage('Contact Us', 'navButton', banner);
 
     tabContainer.append(menuBtn, homeBtn, contactBtn);
 
-
     let mainContent = document.createElement('div');
+    mainContent.classList.add('main-content')
 
     content.appendChild(mainContent);
-    
+    homePage();
 }
 
 export default tabDisplay
