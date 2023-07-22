12f5a615ad6c1d1f6b3d38456842be59e06580ac
Will <willbickham3@gmail.com>
2023-07-22 18:53:10 -0400

    Initialize menu file

---
+++ src/menu.js
@@ -0,0 +1,16 @@
+import paragraph from "./paragraphHolder";
+import createTabsWithImage from "./tabCreater";
+
+
+// paragraph(string) <-- requires string argument but appends paragraph element
+// createTabsWithImage(element, text, classList, ImageSrc) <-- element to be created with inner text, class name and image appended to it
+
+function newMenu(name, price,)
+
+function mama(contentHeader, ) {
+    let contentContainer = document.createElement('div');
+    contentContainer.id.add('content');
+    contentContainer.innerHTML = contentHeader;
+
+    
+}
