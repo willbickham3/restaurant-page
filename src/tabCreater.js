81d8c5a528c5c1474f1d3cf00b88cfc4280190c5
Will <willbickham3@gmail.com>
2023-07-22 18:53:40 -0400

    Rewrite for more reusability

--- src/tabCreater.js
+++ src/tabCreater.js
@@ -1,13 +1,13 @@
-function createTabsWithImage(text, classList, ImageSrc) {
-    const button = document.createElement('button');
-    button.innerHTML = text;
-    button.classList.add(classList);
+function createTabsWithImage(element, text, classList, ImageSrc) {
+    const tab = document.createElement(element);
+    tab.innerHTML = text;
+    tab.classList.add(classList);
 
     const image = document.createElement('img');
     image.src = ImageSrc;
 
-    button.appendChild(image);
-    return button
+    tab.appendChild(image);
+    return tab
 }
 
 export default createTabsWithImage
