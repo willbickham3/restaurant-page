264ed6607ffbd7fee408e59dd892fb660190f80d
Will <willbickham3@gmail.com>
2023-07-21 23:21:56 -0400

    Add strings for use in paragraph function

--- src/home.js
+++ src/home.js
@@ -1,5 +1,10 @@
 import orcCook from './images/Zug-Zug.jpg'
 import robbyflay from './images/robby-flay.jpg'
+import paragraph from './paragraphHolder';
+
+const string1 = `In the lively port town of Booty Bay, Chef Zug, a burly orc with a passion for transforming wild game into soulful dishes, met Chef Robby Flay, a human with an innate flair for innovative culinary creations. Despite their diverse backgrounds, they bonded over their shared love for cooking. Zug, raised in the harsh wilderness of Durotar, developed a reputation among his tribe for his hearty and rustic dishes, while Robby honed his skills in Stormwind, blending flavors with finesse.`;
+const string2 = `Tasting each other's creations, they realized the potential of their combined talents. Together, they opened a dynamic restaurant that celebrated the best of both their culinary worlds. Chef Zug's signature "Zug's Wild Stew," featuring tender game meat and aromatic herbs, harmoniously complemented Chef Robby Flay's "Flay's Fusion Medley," a delightful blend of Eastern Kingdoms' flavors with an innovative touch.`;
+const string3 = `Their restaurant in Booty Bay became a melting pot of tastes, drawing patrons from all corners of Azeroth. As word spread of their unique alliance and culinary harmony, Zug and Flay earned fame as ambassadors of culinary unity, bringing together the Horde and the Alliance through the language of food.`;
 
 function homePage() {
     const greetingDiv = document.createElement('div');
@@ -19,7 +24,10 @@ function homePage() {
 
     document.querySelector('#content').appendChild(greetingDiv);
     imageContainer.append(humanImage, orcImage)
-    greetingDiv.appendChild(imageContainer);
+    const introduction = document.createElement('div');
+    introduction.classList.add('sub-section')
+    introduction.append(paragraph(string1), paragraph(string2), paragraph(string3))
+    greetingDiv.append(imageContainer, introduction);
 }
 
 export default homePage
