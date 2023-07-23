a99b4b10e6518bc1f1eb7e7c444191a1a2104405
Will <willbickham3@gmail.com>
2023-07-22 23:49:02 -0400

    Add click events for buttons

--- src/startup.js
+++ src/startup.js
@@ -1,6 +1,8 @@
 import createTabsWithImage from './tabCreater'
 import banner from '/src/images/banner2.png'
 import { containerDiv, imageCreater } from './imageCreater';
+import createMenu from './menu';
+import homePage from './home';
 
 // createTabsWithImage takes 3 arguments (inner text of button, class of button, and image src)
 // page load module
@@ -14,11 +16,29 @@ function tabDisplay() {
     document.body.prepend(navHeader)
     document.querySelector('header').appendChild(tabContainer);
 
-    const menuBtn = createTabsWithImage('button', 'Menu', 'navButton', banner);
-    const homeBtn = createTabsWithImage('button', 'Home', 'navButton', banner);
-    const contactBtn = createTabsWithImage('button', 'Contact Us', 'navButton', banner);
+    const menuBtn = createTabsWithImage('button', 'Menu', 'navButton', 'menuBtn', banner);
+    const homeBtn = createTabsWithImage('button', 'Home', 'navButton', 'homeBtn', banner);
+    const contactBtn = createTabsWithImage('button', 'Contact Us', 'navButton', 'contactBtn', banner);
+
+    function menuLoad() {const menuBtn = document.querySelector('#menuBtn');
+menuBtn.addEventListener('click', function () {
+    document.querySelector('.sub-header').remove();
+    createMenu()
+    menuBtn.disabled = true;
+    homeBtn.disabled = false;
+})}
+
+function homeLoad() {const homeBtn = document.querySelector('#homeBtn');
+homeBtn.addEventListener('click', function () {
+    homeBtn.disabled = true;
+    menuBtn.disabled = false;
+    document.querySelector('.menu').remove();
+    homePage();
+})}
 
     tabContainer.append(menuBtn, homeBtn, contactBtn);
+    menuLoad();
+    homeLoad();
 }
 
 export default tabDisplay
