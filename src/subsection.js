b4dca9ad35133f036f931dc02ad0cad049d3bdbc
Will <willbickham3@gmail.com>
2023-07-22 23:01:53 -0400

    Change created element for reusability

--- src/subsection.js
+++ src/subsection.js
@@ -1,5 +1,5 @@
-function subSectionCreater(className, subSect) {
-    const subSection = document.createElement('div');
+function subSectionCreater(element, className, subSect) {
+    const subSection = document.createElement(element);
     subSection.classList.add(className);
     subSection.innerHTML = subSect;
     return subSection
