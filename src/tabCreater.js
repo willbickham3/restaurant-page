5c417f76b672cc77b25a497650ae1edfbf28730f
Will <willbickham3@gmail.com>
2023-07-22 23:49:16 -0400

    Add id argument

--- src/tabCreater.js
+++ src/tabCreater.js
@@ -1,7 +1,8 @@
-function createTabsWithImage(element, text, classList, ImageSrc) {
+function createTabsWithImage(element, text, classList, idName, ImageSrc) {
     const tab = document.createElement(element);
     tab.innerHTML = text;
     tab.classList.add(classList);
+    tab.setAttribute('id', idName);
 
     const image = document.createElement('img');
     image.src = ImageSrc;
