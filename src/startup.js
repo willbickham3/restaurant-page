fb875b107acbc986c4293baaf67dce61995696ca
Will <willbickham3@gmail.com>
2023-07-23 21:41:31 -0400

    Add button functionality as well as import removeChild function

--- src/startup.js
+++ src/startup.js
@@ -1,8 +1,10 @@
-import createTabsWithImage from './tabCreater'
+import createTabsWithImage from './createrFunctions/tabCreater'
 import banner from '/src/images/banner2.png'
-import { containerDiv, imageCreater } from './imageCreater';
-import createMenu from './menu';
-import homePage from './home';
+import { containerDiv, imageCreater } from './createrFunctions/imageCreater';
+import createMenu from './pageBuilders/menu';
+import homePage from './pageBuilders/home';
+import contactUs from './pageBuilders/contact';
+import removeChildEle from './createrFunctions/removeChild';
 
 // createTabsWithImage takes 3 arguments (inner text of button, class of button, and image src)
 // page load module
@@ -20,25 +22,40 @@ function tabDisplay() {
     const homeBtn = createTabsWithImage('button', 'Home', 'navButton', 'homeBtn', banner);
     const contactBtn = createTabsWithImage('button', 'Contact Us', 'navButton', 'contactBtn', banner);
 
-    function menuLoad() {const menuBtn = document.querySelector('#menuBtn');
-menuBtn.addEventListener('click', function () {
-    document.querySelector('.sub-header').remove();
-    createMenu()
-    menuBtn.disabled = true;
-    homeBtn.disabled = false;
-})}
-
-function homeLoad() {const homeBtn = document.querySelector('#homeBtn');
-homeBtn.addEventListener('click', function () {
-    homeBtn.disabled = true;
-    menuBtn.disabled = false;
-    document.querySelector('.menu').remove();
-    homePage();
-})}
-
     tabContainer.append(menuBtn, homeBtn, contactBtn);
+
+function menuLoad() {const menuBtn = document.querySelector('#menuBtn');
+    menuBtn.addEventListener('click', function () {
+        removeChildEle(content);
+        createMenu()
+        menuBtn.disabled = true;
+        homeBtn.disabled = false;
+        contactBtn.disabled = false;
+})}
+
+function homeLoad() {
+    const homeBtn = document.querySelector('#homeBtn');
+        homeBtn.addEventListener('click', function () {
+            removeChildEle(content);
+        homePage();
+        homeBtn.disabled = true;
+        menuBtn.disabled = false;
+        contactBtn.disabled = false
+})}
+
+function contactLoad() {
+    const contactBtn = document.querySelector('#contactBtn');
+        contactBtn.addEventListener('click', function () {
+            removeChildEle(content);
+        contactUs();
+        homeBtn.disabled = false;
+        menuBtn.disabled = false;
+        contactBtn.disabled = true
+})}
+
     menuLoad();
     homeLoad();
+    contactLoad();
 }
 
 export default tabDisplay
